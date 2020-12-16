89b869804658a0a49f2d93f008f0890527bdfe63
Jeff MacDonald <jam@zoidtechnologies.com>
2020-12-15 19:50:53 -0500

    sql/player.sql: added 'datelastplayedepoch' attribute.

--- sql/player.sql
+++ sql/player.sql
@@ -3,6 +3,7 @@ create or replace view empire.player as
         *,
         (attributes->>'memberid')::bigint as memberid,
         (attributes->>'datelastplayed')::timestamptz as datelastplayed,
+        extract(epoch from (attributes->>'datelastplayed')::timestamptz) as datelastplayedepoch,
         (attributes->>'name')::text as name
     from engine.node
     where attributes ? 'memberid' and attributes ? 'grain' and attributes ? 'land'-- and attributes ? 'mills'
