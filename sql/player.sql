3d8403cf7ee41dcf55759a4a8ee064a7d5355817
Jeff MacDonald <jam@zoidtechnologies.com>
2021-06-17 00:15:32 -0400

    empyre/sql/player.sql: revise player view.

--- sql/player.sql
+++ sql/player.sql
@@ -1,12 +1,18 @@
+--create table if not exists empyre.__player (
+--    "id" bigserial unique primary key not null,
+--    "memberid" bigint constraint fk_engine_node_createdbyid references engine.__member(id) on update cascade on delete set null,
+--    "attributes" jsonb,
+--   "datelastplayed" timestamptz
+--);
+
 create or replace view empyre.player as
     select
-        *,
+        n.*,
+        extract(epoch from (n.attributes->>'datelastplayed')::timestamptz) as datelastplayedepoch,
         (attributes->>'memberid')::bigint as memberid,
-        (attributes->>'datelastplayed')::timestamptz as datelastplayed,
-        extract(epoch from (attributes->>'datelastplayed')::timestamptz) as datelastplayedepoch,
-        (attributes->>'name')::text as name
-    from engine.node
-    where attributes ? 'memberid' and attributes ? 'grain' and attributes ? 'land'-- and attributes ? 'mills'
+        (attributes->>'playername') as playername
+    from engine.__node as n
+    where attributes ? 'playername' and attributes ? 'datelastplayedepoch' and attributes ? 'memberid' and attributes->>'prg'='empyre'
 ;
 
 grant select on empyre.player to apache;
