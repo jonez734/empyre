253bc2ff934537b0a3e61202f87d45a45a14659b
Jeff MacDonald <jam@zoidtechnologies.com>
2022-08-01 19:00:15 -0400

    - empyre/empyremodules/maint.py: reworked edit function so it does not rely on p.isdirty() - most of this modules functions have been moved to submodules - in 'your status', if player is SYSOP, show a prompt for the player to show - return True

--- empyremodules/maint.py
+++ empyremodules/maint.py
@@ -12,7 +12,7 @@ def init(args, **kw):
 
 def main(args, **kw):
     player = kw["player"] if "player" in kw else None
-
+    sysop = bbsengine.checksysop(args)
     done = False
     while not done:
         bbsengine.title("maint")
@@ -39,28 +39,22 @@ def main(args, **kw):
             continue
         elif ch == "E":
             ttyio.echo("Edit Player's Profile")
-            playername = inputplayername("player name: ", player.name, args=args)
-            playerid = getplayerid(args, playername)
+            playername = lib.inputplayername("player name: ", player.name, args=args)
+            playerid = lib.getplayerid(args, playername)
             if playerid is None:
                 continue
             p = lib.Player(args)
             p.load(playerid)
+            lib.setarea(args, p, "edit player %r" % (playername))
             p.edit()
-            if p.isdirty() is True:
-                if ttyio.inputboolean("save? [Yn]: ", "Y") is True:
-                    p.save()
-                    if player.memberid == p.memberid:
-                        ttyio.echo("player.memberid is p.memberid")
-                        player = p
-            else:
-                if ttyio.inputboolean("save? [yN]: ", "N") is True:
-#                    p.save()
-                    if player.memberid == p.memberid:
-                        ttyio.echo("player.memberid is p.memberid")
-                        player = p
+            p.adjust()
+            p.status()
+            lib.setarea(args, p, "edit player %r" % (playername))
+            if ttyio.inputboolean("save? [Yn]: ", "Y") is True:
+                p.save()
         elif ch == "L":
             ttyio.echo("List Players")
-            otherplayers(args, player)
+            lib.runsubmodule(args, player, "listplayers")
         elif ch == "P":
             ttyio.echo("Play Empyre")
             if lib.runsubmodule(args, player, "play") is not True:
@@ -68,15 +62,19 @@ def main(args, **kw):
 #            play(args, player)
         elif ch == "R":
             ttyio.echo("Reset Empyre")
-            resetempire(args, player)
+            lib.runsubmodule(args, player, "resetempyre")
         elif ch == "S":
             ttyio.echo("Scratch News")
-            scratchnews(args, player)
+            lib.runsubmodule(args, player, "scratchnews")
         elif ch == "X":
             ttyio.echo("bbs credit -> empyre coin exchange rate, not implemented")
         elif ch == "Y":
-            ttyio.echo("Player Stats")
-            playername = lib.inputplayername("player: ", "", verify=verifyPlayerNameFound, args=args)
+            if sysop is True:
+                ttyio.echo("Player Stats")
+                playername = lib.inputplayername("player: ", "", verify=lib.verifyPlayerNameFound, args=args)
+            else:
+                ttyio.echo("Your stats")
+                playername = player.name
             playerid = lib.getplayerid(args, playername)
             if playerid is None:
                 ttyio.echo("%s not found." % (playername), level="error")
@@ -86,4 +84,4 @@ def main(args, **kw):
             p.status()
 
     ttyio.echo()
-    return
+    return True
