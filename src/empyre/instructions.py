6b1edef30eea25979828695790f443310081ac95
Jeff MacDonald <jam@zoidtechnologies.com>
2022-09-05 19:09:17 -0400

    - empyre/instructions.py: added 'init' and 'access' functions as required by submodule protocol

--- src/empyre/instructions.py
+++ src/empyre/instructions.py
@@ -3,6 +3,12 @@ import bbsengine5 as bbsengine
 # from . import lib
 from . import data
 
+def init(args, **kw):
+    return True
+
+def access(args, **kw):
+    return True
+
 def main(args, **kw):
     bbsengine.filedisplay(args, data.get("instructions.txt"))
 #    bbsengine.filedisplay(args, bbsengine.buildfilepath(lib.DATADIR, "instructions.txt"))
