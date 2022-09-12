d0a0769c734122f5ccd9757ee761abb777c9e6e0
Jeff MacDonald <jam@zoidtechnologies.com>
2022-09-11 21:40:36 -0400

    - empyre/src/empyre/drydock.py: fixed up access() prototype; started on main()

--- src/empyre/drydock.py
+++ src/empyre/drydock.py
@@ -9,8 +9,39 @@ from . import lib
 def init(args, **kw):
     return
 
-def main(args, **kw):
-    return
+def access(args, op, **kw):
+    return True
 
-def access(args, **kw):
+def main(args, **kw):
+    player = kw["player"] if "player" in kw else None
+
+    if player is None:
+        ttyio.echo("you do not exist! go away!", level="error")
+        return False
+
+    loop = True
+    while loop:
+        bbsengine.title("dry dock")
+        lib.setarea(args, player, "dry dock")
+
+        ttyio.echo("You have %s and %s" % (bbsengine.pluralize(player.ships, "ship", "ships"), bbsengine.pluralize(player.navigators, "navigator", "navigators")))
+        ttyio.echo(":compass: [R]ecruit navigator") # show how many are needed per ship
+        ttyio.echo(":package: [E]xports")
+        ttyio.echo(":anchor: [S]hips")
+        ttyio.echo("{f6} [Q]uit")
+        ch = ttyio.inputchar("dry dock: ", "RESQ", "Q")
+        if ch == "Q":
+            loop = False
+            break
+        elif ch == "E":
+            ttyio.echo("Exports")
+        elif ch == "R":
+            ttyio.echo("Recuit Navigator")
+            if player.navigators <= player.ships:
+                need = abs(player.ships - player.navigators)
+                if need > 0:
+                    ttyio.echo("You need %s to outfit your %s." % (bbsengine.pluralize(player.navigators, "navigator", "navigators"), bbsengine.pluralize(player.ships, "ship", "ships")))
+        elif ch == "S":
+            ttyio.echo("Ships")
+
     return True
