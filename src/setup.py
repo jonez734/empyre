95d69cdf183be98041a98365460e437292b85c4c
Jeff MacDonald <jam@zoidtechnologies.com>
2022-09-11 21:14:40 -0400

    - empyre/src/setup.py: changed author arg

--- src/setup.py
+++ src/setup.py
@@ -9,13 +9,13 @@ v = time.strftime("%Y%m%d%H%M")
 
 projectname = "empyre"
 
-os.system("./updateversion.sh")
+# os.system("./updateversion.sh") use Makefile!
 
 setup(
   name=projectname,
   version=v,
   url="https://repo.zoidtechnologies.com/%s/" % (projectname),
-  author="zoid technologies",
+  author="zoidtechnologies.com",
   author_email="%s@projects.zoidtechnologies.com" % (projectname),
   requires=["ttyio5", "bbsengine5"],
   scripts=["bin/empyre"],
