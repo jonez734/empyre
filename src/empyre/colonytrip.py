feb4bbd057bb7cf35537f725379844d1e7b835d9
Jeff MacDonald <jam@zoidtechnologies.com>
2023-01-09 19:41:25 -0500

    - empyre/colonytrip.py: changed init() to return True; added access() which always returns True; added check of player for None

--- src/empyre/colonytrip.py
+++ src/empyre/colonytrip.py
@@ -4,10 +4,17 @@ import ttyio5 as ttyio
 import bbsengine5 as bbsengine
 
 def init(args, **kw):
-    pass
+    return True
+
+def access(args, op, **kwargs):
+    return True
 
 def main(args, **kw):
     player = kw["player"] if "player" in kw else None
+    if player is None:
+        ttyio.echo("you do not exist! go away!", level="error")
+        return False
+
     ttyio.echo("colony trip... %s{f6}" % (bbsengine.pluralize(player.colonies, "colony", "colonies")))
     if player.colonies > 0:
         ttyio.echo("King George wishes you a safe and prosperous trip to your %s{f6}" % (bbsengine.pluralize(player.colonies, "colony", "colonies", quantity=False)))
