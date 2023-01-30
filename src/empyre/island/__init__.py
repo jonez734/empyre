9d35f54d348e1835cd3636621b2c10f6d2ea2733
Jeff MacDonald <jam@zoidtechnologies.com>
2023-01-29 21:18:11 -0500

    - empyre/island/__init__.py: added

---
+++ src/empyre/island/__init__.py
@@ -0,0 +1,10 @@
+def init(args, **kwargs):
+    return True
+
+def access(args, op, **kwargs):
+    return True
+
+def main(args, **kwargs):
+    bbsengine.title("island")
+    ttyio.echo("under construction")
+    return True
