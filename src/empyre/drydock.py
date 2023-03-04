62a61771967b55c98956519ee26d108d0ec97e10
Jeff MacDonald <jam@zoidtechnologies.com>
2023-03-04 18:58:29 -0500

    - empyre/drydock.py: added 'help' function and a call to it in inputchar() call; added a call to lib.trade() for ships

--- src/empyre/drydock.py
+++ src/empyre/drydock.py
@@ -7,7 +7,7 @@ import bbsengine5 as bbsengine
 from . import lib
 
 def init(args, **kw):
-    return
+    return True
 
 def access(args, op, **kw):
     return True
@@ -18,18 +18,19 @@ def main(args, **kw):
     if player is None:
         ttyio.echo("you do not exist! go away!", level="error")
         return False
+    def help():
+        ttyio.echo(":compass: {var:optioncolor}[R]{var:labelcolor}ecruit navigator{var:normalcolor}") # show how many are needed per ship
+        ttyio.echo(":package: {var:optioncolor}[E]{var:labelcolor}xports{var:normalcolor}")
+        ttyio.echo(":anchor: {var:optioncolor}[S]{var:labelcolor}hips{var:normalcolor}")
+        ttyio.echo("{f6}:door: {var:optioncolor}[Q]{var:labelcolor}uit{var:normalcolor}")
 
     loop = True
     while loop:
         bbsengine.title("dry dock")
         lib.setarea(args, player, "dry dock")
-
+        help()
         ttyio.echo("You have %s and %s" % (bbsengine.pluralize(player.ships, "ship", "ships"), bbsengine.pluralize(player.navigators, "navigator", "navigators")))
-        ttyio.echo(":compass: {var:optioncolor}[R]{var:labelcolor}ecruit navigator{var:normalcolor}") # show how many are needed per ship
-        ttyio.echo(":package: {var:optioncolor}[E]{var:labelcolor}xports{var:normalcolor}")
-        ttyio.echo(":anchor: {var:optioncolor}[S]{var:labelcolor}hips{var:normalcolor}")
-        ttyio.echo("{f6}:door: {var:optioncolor}[Q]{var:labelcolor}uit{var:normalcolor}")
-        ch = ttyio.inputchar("{var:promptcolor}dry dock: {var:inputcolor}", "RESQ", "Q")
+        ch = ttyio.inputchar("{var:promptcolor}dry dock: {var:inputcolor}", "RESQ", "Q", help=help)
         if ch == "Q":
             loop = False
             break
@@ -43,5 +44,6 @@ def main(args, **kw):
                     ttyio.echo("You need %s to outfit your %s." % (bbsengine.pluralize(player.navigators, "navigator", "navigators"), bbsengine.pluralize(player.ships, "ship", "ships")))
         elif ch == "S":
             ttyio.echo("Ships")
+            lib.trade(args, player, "ships", "ships", price=5000, singular="ship", plural="ships", emoji=":anchor:")
 
     return True
