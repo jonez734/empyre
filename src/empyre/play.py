f9bfe1b115b98150fad749d949dbfd96ec7d4657
Jeff MacDonald <jam@zoidtechnologies.com>
2023-04-14 16:21:21 -0400

    - empyre/play.py: changed submodules that are run for debugging; init() must always return a boolean

--- src/empyre/play.py
+++ src/empyre/play.py
@@ -8,7 +8,7 @@ import bbsengine5 as bbsengine
 from . import lib # this means "import lib from the current package" also allows .. and ...
 
 def init(args, **kwargs):
-    pass
+    return True
 
 def main(args, **kw):
     player = kw["player"] if "player" in kw else None
@@ -23,22 +23,28 @@ def main(args, **kw):
     if player.turncount >= lib.TURNSPERDAY:
         ttyio.echo("{red}The other rulers unite against you for hogging the game!{/all}")
         player.turncount = lib.TURNSPERDAY
+        player.save()
         return True
     
     player.turncount += 1
     player.adjust()
     player.save()
 
-#    for x in ("drydock",):
-    for x in ("weather", "disaster", "harvest", "colonytrip", "town", "combat", "drydock", "quests", "investments"):
+    for x in ("drydock",):
+#    for x in ("quests",):
+#    for x in ("combat", ):
+        if args.debug is True:
+            ttyio.echo(f"play.100: x={x!r}", level="debug")
+#    for x in ("weather", "disaster", "harvest", "colonytrip", "town", "combat", "drydock", "quests", "investments"):
         if lib.runsubmodule(args, player, x) is False:
             ttyio.echo("error running submodule %r" % (x), level="error")
+        player.adjust()
         player.save()
 
     ttyio.echo("end turn...{F6}")
     
     if player.serfs < 100:
-        ttyio.echo("{green}You haven't enough serfs to maintain the empyre! It's turned over to King George and you are {yellow}beheaded{/fgcolor}{green}.{/all}")
+        ttyio.echo("{var:normalcolor}You haven't enough serfs to maintain the empyre! It's turned over to King George and you are {var:highlightcolor}beheaded{var:normalcolor}.{/all}")
         player.memberid = None
         player.beheaded = True
         player.save()
