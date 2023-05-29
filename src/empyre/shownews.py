49e608f14a428bd52c2e123025776fa55517a697
Jeff MacDonald <jam@zoidtechnologies.com>
2023-05-29 19:38:34 -0400

    - empyre/shownews.py: added proper access() and init() functions; updated main() to accept kwargs; updated sql query in main() so it uses dateupdated or datecreated on the newsentry

--- src/empyre/shownews.py
+++ src/empyre/shownews.py
@@ -1,10 +1,17 @@
 import ttyio5 as ttyio
 import bbsengine5 as bbsengine
 
-def main(args:object, player:object):
+def access(args, op, **kw):
+    return True
+
+def init(args, **kw):
+    return True
+
+def main(args:object, player:object, **kw):
     dbh = bbsengine.databaseconnect(args)
-    sql = "select * from empyre.newsentry where (extract(epoch from (coalesce(dateupdated, datecreated)))) > %s"
-    dat = (player.datelastplayedepoch,)
+    sql = "select * from empyre.newsentry where coalesce(dateupdated, datecreated) > %s"
+#    sql = "select * from empyre.newsentry where (extract(epoch from (coalesce(dateupdated, datecreated)))) > %s"
+    dat = (player.datelastplayed,)
     cur = dbh.cursor()
     cur.execute(sql, dat)
     for entry in bbsengine.resultiter(cur, 10): # iter_news(cur, 10):
@@ -12,4 +19,3 @@ def main(args:object, player:object):
         
     ttyio.echo("{/all}")
     return True
-
