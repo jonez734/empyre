6254cd3139bb32955b7c3374c13f3340a741e148
Jeff MacDonald <jam@zoidtechnologies.com>
2023-08-02 19:03:00 -0400

    - empyre/loadship.py: added access(), and added a call to bbsengine.util.heading()

--- src/empyre/loadship.py
+++ src/empyre/loadship.py
@@ -1,10 +1,14 @@
-import ttyio5 as ttyio
-import bbsengine5 as bbsengine
+import ttyio6 as ttyio
+import bbsengine6 as bbsengine
 
 from . import lib
 
 def init(args, **kw):
     return
 
+def access(args, op, **kw):
+    return True
+
 def main(args, **kw):
+    bbsengine.util.heading("load ship")
     return
