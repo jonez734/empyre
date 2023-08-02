f274a184fed35a5cd95249594d988141d344b794
Jeff MacDonald <jam@zoidtechnologies.com>
2023-08-02 19:12:02 -0400

    - empyre/drydock.py: cannot by ships wo a shipyard (this check needs to be better-- 10 ships per shipyard); cannot buy a navigator without a ship.

--- src/empyre/drydock.py
+++ src/empyre/drydock.py
@@ -3,7 +3,6 @@
 import ttyio5 as ttyio
 import bbsengine5 as bbsengine
 
-
 from . import lib
 
 def init(args, **kw):
@@ -18,32 +17,38 @@ def main(args, **kw):
     if player is None:
         ttyio.echo("you do not exist! go away!", level="error")
         return False
-    def help():
+    def help(**kw):
         ttyio.echo(":compass: {var:optioncolor}[R]{var:labelcolor}ecruit navigator{var:normalcolor}") # show how many are needed per ship
         ttyio.echo(":package: {var:optioncolor}[E]{var:labelcolor}xports{var:normalcolor}")
         ttyio.echo(":anchor: {var:optioncolor}[S]{var:labelcolor}hips{var:normalcolor}")
         ttyio.echo("{f6}:door: {var:optioncolor}[Q]{var:labelcolor}uit{var:normalcolor}")
 
-    loop = True
-    while loop:
+    done = False
+    while not done:
         bbsengine.title("dry dock")
         lib.setarea(args, player, "dry dock")
         help()
         ttyio.echo("You have %s and %s" % (bbsengine.pluralize(player.ships, "ship", "ships"), bbsengine.pluralize(player.navigators, "navigator", "navigators")))
         ch = ttyio.inputchar("{var:promptcolor}dry dock: {var:inputcolor}", "RESQ", "Q", help=help)
         if ch == "Q":
-            loop = False
+            done = True
             break
         elif ch == "E":
-            ttyio.echo("Exports")
+            ttyio.echo("Exports{/all}")
         elif ch == "R":
-            ttyio.echo("Recuit Navigator")
-            if player.navigators <= player.ships:
+            ttyio.echo("Recuit Navigator{/all}")
+            if player.ships == 0:
+                ttyio.echo("You do not have any ships.")
+            elif player.navigators <= player.ships:
                 need = abs(player.ships - player.navigators)
                 if need > 0:
-                    ttyio.echo("You need %s to outfit your %s." % (bbsengine.pluralize(player.navigators, "navigator", "navigators"), bbsengine.pluralize(player.ships, "ship", "ships")))
+                    ttyio.echo("You need %s to outfit your %s." % (bbsengine.util.pluralize(player.navigators, "navigator", "navigators"), bbsengine.util.pluralize(player.ships, "ship", "ships")))
+                    lib.trade(args, player, "navigators", "navigators", price=500, singular="navigator", plural="navigators", emoji=":compass:")
         elif ch == "S":
-            ttyio.echo("Ships")
-            lib.trade(args, player, "ships", "ships", price=5000, singular="ship", plural="ships", emoji=":anchor:")
+            ttyio.echo("Ships{/all}")
+            if player.shipyards > 0:
+                lib.trade(args, player, "ships", "ships", price=5000, singular="ship", plural="ships", emoji=":anchor:")
+            else:
+                ttyio.echo("You do not have any shipyards.")
 
     return True
