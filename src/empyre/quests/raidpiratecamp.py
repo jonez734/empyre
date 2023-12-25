3a1b6a48abd3c723164e4f8d954a416a4d339197
Jeff MacDonald <jam@zoidtechnologies.com>
2023-12-24 20:03:10 -0500

    - empyre/quests/raidpiratecamp.py: converted echo() call to f-string, added init(), access(), and buildargs()

--- src/empyre/quests/raidpiratecamp.py
+++ src/empyre/quests/raidpiratecamp.py
@@ -1,16 +1,25 @@
 # @since 20220731 created quests.raidpiratecamp.py
 
+def init(args, **kw):
+    return True
+
+def access(args, op, **kw):
+    return True
+
+def buildargs(args, **kw):
+    return None
+
 def main(args, **kw):
     player = kw["player"] if "player" in kw else None
 
     ttyio.echo("player.coins=%r" % (player.coins), level="debug")
 
     if isquestcompleted() is True:
-        ttyio.echo("You gain :moneybag: %s." % (bbsengine.pluralize(30000, "coin", "coins")))
+        ttyio.echo("You gain {}.".format(bbsengine.pluralize(30000, "coin", "coins", emoji=":moneybag:")))
         player.coins += 30000
         result = True
     else:
         ttyio.echo("You failed to complete this quest.")
         result = False
-    ttyio.echo("after completed check, player.coins=%r" % (player.coins), level="debug")
+    ttyio.echo(f"after completed check, {player.coins=}", level="debug")
     return result
