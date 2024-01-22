0f38e077f5a988cba651d7e58a04b5bc4f41e5ad
Jeff MacDonald <jam@zoidtechnologies.com>
2024-01-22 17:08:19 -0500

    - empyre/town/trainsoldiers.py:   * ttyio -> bbsengine.io   * bbsengine.util -> util

--- src/empyre/town/trainsoldiers.py
+++ src/empyre/town/trainsoldiers.py
@@ -1,7 +1,9 @@
-import ttyio6 as ttyio
-import bbsengine6 as bbsengine
+#import ttyio6 as ttyio
+#import bbsengine6 as bbsengine
 
 from .. import lib
+
+from bbsengine6 import io, util
 
 def init(args, **kwargs):
     return True
@@ -15,13 +17,13 @@ def buildargs(args, **kw):
 # @since 20200830
 # @see https://github.com/Pinacolada64/ImageBBS/blob/master/v1.2/games/empire6/plus_emp6_town.lbl#L244
 def main(args, player, **kwargs):
-    bbsengine.util.heading(": Soldier Training :")
+    util.heading(": Soldier Training :")
     lib.setarea(args, player, "town -> train soldiers")
-    ttyio.echo()
+    io.echo()
     eligible = int(player.nobles*20-player.soldiers)
-    ttyio.echo("empyre.trainsoldiers.100: eligible=%d" % (eligible), level="debug")
+    io.echo("empyre.trainsoldiers.100: eligible=%d" % (eligible), level="debug")
     if player.serfs < 1500 or eligible > (player.serfs // 2):
-        ttyio.echo("You do not have enough serfs of training age.")
+        io.echo("You do not have enough serfs of training age.")
         return True
     
     #&"{f6}{white}You have"+str$(wb)+" serfs that meet the requirement to be trained"
@@ -29,15 +31,15 @@ def main(args, player, **kwargs):
     #&"{f6}{lt. green}Do you want them trained (Y/N) >> ":gosub 1902
     #if a then sf=sf-wb:la=la-wb:wa=wa+wb:&"{f6:2}{pound}w2{yellow}Ok, all serfs have been trained.{f6}{pound}q1"
     # ttyio.echo("{f6}{white}You have %s requirements to be trained %s." % (bbsengine.pluralize(eligible, "serf that meets", "serfs that meet"), bbsengine.pluralize(eligible, "as a soldier", "as soldiers", quantity=False)))
-    ttyio.echo("{f6}{var:normalcolor}You have %s requirments to be trained as %s." % (bbsengine.util.pluralize(eligible, "serf that meets", "serfs that meet"), bbsengine.util.pluralize(eligible, "a :military-helmet: warrior", ":military-helmet: warriors", quantity=False)))
+    io.echo("{f6}{var:normalcolor}You have %s requirments to be trained as %s." % (bbsengine.util.pluralize(eligible, "serf that meets", "serfs that meet"), bbsengine.util.pluralize(eligible, "a :military-helmet: warrior", ":military-helmet: warriors", quantity=False)))
     if eligible > 0:
-        ttyio.echo("Training cost is 1 acre per serf.")
-        if ttyio.inputboolean("{var:promptcolor}Do you wish them trained? {var:optioncolor}[yN]{var:promptcolor}: {var:inputcolor}", "N") is True:
+        io.echo("Training cost is 1 acre per serf.")
+        if io.inputboolean("{var:promptcolor}Do you wish them trained? {var:optioncolor}[yN]{var:promptcolor}: {var:inputcolor}", "N") is True:
             player.serfs -= eligible
             player.land -= eligible
             player.soldiers += eligible
-            ttyio.echo("Promotions completed.")
+            io.echo("Promotions completed.")
         else:
-            ttyio.echo("No promotions performed.")
+            io.echo("No promotions performed.")
 
     return True
