7e10f26f07e8581c75bbe226c17758432ec7eb14
Jeff MacDonald <jam@zoidtechnologies.com>
2024-01-27 02:58:02 +0000

    - empyre/sql/ship.sql: ownerid -> playerid; added 'kind'; added 'createdby' and 'updatedby'

--- src/empyre/sql/ship.sql
+++ src/empyre/sql/ship.sql
@@ -1,21 +1,25 @@
 -- @since 20230824
 create table if not exists empyre.__ship (
     "name" text unique not null primary key,
-    "ownerid" bigint constraint fk_empyre_ship_ownerid references empyre.__player(id) on update cascade on delete set null,
+    "playerid" bigint constraint fk_empyre_ship_playerid references empyre.__player(id) on update cascade on delete set null,
     "location" text,
     "status" text,
     "manifest" jsonb,
     "navigator" boolean,
-    "datecreated" timestamptz
+    "kind" text, # @ty tmovacik "cargo" first, then "passengers" (dad)
+    "datecreated" timestamptz,
+    "createdbyid" bigint constraint fk_empyre_ship_createdbyid references engine.__member(id) on update cascade on delete set null,
+    "dateupdated" timestamptz,
+    "updatedbyid" bigint constraint fk_empyre_ship_updatedbyid references engine.__member(id) on update cascade on delete set null
 );
 
 -- @since 20230716
 create or replace view empyre.ship as
     select
         s.*,
-        owner.moniker as ownermoniker
+        player.moniker as playermoniker
     from empyre.__ship as s
-    left join empyre.__player as owner on (owner.id = s.ownerid)
+    left join empyre.__player as player on (player.id = s.playerid)
 ;
 
 grant select on empyre.ship to :web, :bbs;
