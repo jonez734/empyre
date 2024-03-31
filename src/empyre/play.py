c1fd1502abd2f0b037be58b6ad5b9e50ac9900b9
Jeff MacDonald <jam@zoidtechnologies.com>
2024-03-31 17:46:10 -0400

    - empyre/play.py:   * upgraded to bbsengine6   * adjusted the module list for debugging

--- src/empyre/play.py
+++ src/empyre/play.py
@@ -1,59 +1,77 @@
 import time
 
 from datetime import datetime
+import dateutil.tz
 
-import ttyio5 as ttyio
-import bbsengine5 as bbsengine
+from bbsengine6 import io
 
 from . import lib # this means "import lib from the current package" also allows .. and ...
 
 def init(args, **kwargs):
     return True
 
+def access(args, op, **kw):
+    return True
+
+def buildargs(args, **kw):
+    return None
+
 def main(args, **kw):
     player = kw["player"] if "player" in kw else None
-#    player.datelastplayedepoch = time.mktime(time.localtime())
-    player.datelastplayed = datetime.now().isoformat()
-    
-    if lib.runsubmodule(args, player, "sysopoptions") is not True:
-        ttyio.echo("error running module 'sysopoptions'", level="error")
+    if player is None:
+        io.echo("You do not exist! Go Away!")
+        return False
 
-    ttyio.echo("{f6}{cyan}it is a new year...{/all}")
+#    player.datelastplayedepoch = time.mktime(time.localtime())
+    tzlocal = dateutil.tz.tzlocal()
+    player.datelastplayed = datetime.now(tzlocal)
+    
+    if lib.runmodule(args, "sysopoptions", **kw) is False:
+        io.echo("error running module 'sysopoptions'", level="warn")
+
+    io.echo("{f6}{cyan}it is a new year...{/all}")
 
     if player.turncount >= lib.TURNSPERDAY:
-        ttyio.echo("{red}The other rulers unite against you for hogging the game!{/all}")
+        io.echo("{red}The other rulers unite against you for hogging the game!{/all}")
         player.turncount = lib.TURNSPERDAY
         player.save()
         return True
     
+#    if player.coins is not None:
+#        coins = player.getresource("coins")
+#        coins["value"] = player.coins
+#        player.coins = None
     player.turncount += 1
     player.adjust()
     player.save()
+    io.echo(f"{player.datelastplayed=}", level="debug")
 
-    for x in ("weather", "disaster", "harvest", "colonytrip", "town", "combat", "drydock", "quests", "investments"):
+    for x in ("dock",): # town.naturaldisasterbank",): # "yearlyreport",):
+#    for x in ("weather", "disaster", "harvest", "colonytrip", "town", "combat", "dock", "quests", "investments"):
         if args.debug is True:
-            ttyio.echo(f"play.100: x={x!r}", level="debug")
-        if lib.runsubmodule(args, player, x) is False:
-            ttyio.echo("error running submodule %r" % (x), level="error")
+            io.echo(f"play.100: {x=}", level="debug")
+        if lib.runmodule(args, x, **kw) is False:
+            io.echo("error running submodule %r" % (x), level="error")
         player.adjust()
         player.save()
 
-    ttyio.echo("end turn...{F6}")
+    io.echo("end turn...{F6}")
     
     if player.serfs < 100:
-        ttyio.echo("{var:normalcolor}You haven't enough serfs to maintain the empyre! It's turned over to King George and you are {var:highlightcolor}beheaded{var:normalcolor}.{/all}")
-        player.memberid = None
         player.beheaded = True
-        player.save()
+        player.memberid = None
+        player.adjust()
+        player.save(force=True)
+        io.echo("{normalcolor}You haven't enough serfs to maintain the empyre! It's turned over to King George and you are {highlightcolor}beheaded{normalcolor}.{/all}")
         return
-        
+
     player.adjust()
     player.save()
 
-    if lib.runsubmodule(args, player, "yearlyreport") is not True:
-        ttyio.echo("error running 'yearlyreport' submodule.", level="debug")
+    if lib.runmodule(args, "yearlyreport", **kw) is not True:
+        io.echo("error running 'yearlyreport' submodule.", level="debug")
     
-    ttyio.echo("turn complete!", level="success")
-    ttyio.echo("{/all}")
+    io.echo("turn complete!", level="success")
+    io.echo("{/all}")
 
     return True
