7b81bc2b03acfeffec30eb2c7f7055b7e6627967
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-09 19:50:52 -0400

    - empyre/town/naturaldisasterbank.py:   * lib.setarea() now accepts player as a kwarg instead of a positional   * colorized a little   * use player.getresource('coins') instead of hard-coding util.pluralize() calls

--- src/empyre/town/naturaldisasterbank.py
+++ src/empyre/town/naturaldisasterbank.py
@@ -1,5 +1,3 @@
-#import ttyio6 as ttyio
-#import bbsengine6 as bbsengine
 from bbsengine6 import io, util, member
 
 from .. import lib
@@ -19,34 +17,34 @@ def buildargs(args, **kw):
 # @see https://github.com/Pinacolada64/ImageBBS/blob/master/v1.2/games/empire6/plus_emp6_town.lbl#L293
 def main(args, player, **kwargs):
     util.heading("natural disaster bank") # , titlecolor="{bggray}{white}", hrcolor="{darkgreen}")
-    lib.setarea(args, player, "natural disaster bank") # "{bggray}{white}%s{/all}" % ("bank".ljust(terminalwidth)))
+    lib.setarea(args, "natural disaster bank", player=player) # "{bggray}{white}%s{/all}" % ("bank".ljust(terminalwidth)))
+    coinres = player.getresource("coins")
     io.echo()
     exchangerate = 3 #:1 -- 3 coins per credit
     cr = member.getcredits(args)
-    buf = "You have {empyre.highlightcolor}%s{/all} and {empyre.highlightcolor}%s{/all}" % (util.pluralize(player.coins, "coin", "coins", emoji=":moneybag:"), util.pluralize(credits, "credit", "credits"))
-    io.echo(buf)
-    if cr is not None and credits > 0:
-        io.echo("The exchange rate is {empyre.highlightcolor}%s per credit{/all}.{F6}"  % (util.pluralize(exchangerate, "coin", "coins", emoji=":moneybag:")))
-        amount = io.inputinteger("{promptcolor}Exchange how many credits?: {inputcolor}")
+    io.echo(f"{{var:labelcolor}}You have {{var:valuecolor}}{util.pluralize(player.coins, **coinres)}{{var:labelcolor}} and {{var:valuecolor}}{util.pluralize(credits, 'credit', 'credits', emoji=':moneybag:')}{{/all}}")
+    if cr is not None and cr > 0:
+        io.echo(f"{{var:labelcolor}}The exchange rate is {{var:valuecolor}}{util.pluralize(exchangerate, **coinres)} per credit{{/all}}.{{F6}}")
+        amount = io.inputinteger(f"{var:promptcolor}Exchange how many credits?: {var:inputcolor}")
         io.echo("{/all}")
     else:
         io.echo("You have no credits")
         return
+
     if amount is None or amount < 1:
         return
 
     if args.debug is True:
         io.echo(f"empyre.town.bank.100: {credits=}", level="debug")
 
-    if amount > credits:
-        io.echo("Get REAL! You only have {empyre.highlightcolor} %s {/all}!" % (util.pluralize(credits, "credit", "credits")))
+    if amount > cr:
+        io.echo(f"{{var:labelcolor}}Get REAL! You only have {{var:valuecolor}}{util.pluralize(cr, **coinres)}{{var:labelcolor}}!")
         return
 
-    cr -= amount
     player.coins += amount*exchangerate
+    cr -= amount
+    member.setcredits(args, player.moniker, cr, **kwargs)
 
-    member.setcredits(args, player.memberid, cr)
-
-    io.echo("{normalcolor}You now have {valuecolor}%s{normalcolor} and {valuecolor}{empyre.highlightcolor}%s{normalcolor}" % (util.pluralize(player.coins, "coin", "coins", emoji=":moneybag:"), util.pluralize(credits, "credit", "credits")))
-    lib.setarea(args, player, "natural disaster bank")
+    io.echo(f"{{var:normalcolor}}You now have {{var:valuecolor}}{util.pluralize(player.coins, **coinres)}{{var:normalcolor}} and {{var:valuecolor}}{util.pluralize(credits, 'credit', 'credits', emoji=':moneybag:')}{{var:normalcolor}}")
+    lib.setarea(args, "natural disaster bank", player=player)
     return True
