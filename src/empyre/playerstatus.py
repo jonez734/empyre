4e49401ffc7339a30397d1c9bc5070e4a8e35b96
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-14 00:17:22 -0400

    - empyre/src/playerstatus.py: updated main(); added access() and buildargs()

--- src/empyre/playerstatus.py
+++ src/empyre/playerstatus.py
@@ -1,7 +1,16 @@
 def init(args, **kw):
-    pass
+    return True
 
-def main(args, **kw):
-    player = kw["player"] if "player" in kw else None
+def access(args, op, **kwargs):
+    return True
+
+def buildargs(args, **kwargs):
+    return None
+
+def main(args, **kwargs):
+    player = kwargs.get("player", None)
+    if player is None:
+        io.echo(f"empyre.playerstatus.100: {player=}", level="error")
+        return False
     player.status()
     return True
