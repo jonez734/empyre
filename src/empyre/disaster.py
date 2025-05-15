6b2fc9715d72a9181f084e6e3fc0ef85daf1d7dd
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-15 17:00:42 -0400

    - empyre/disaster.py:   * ttyio.echo() -> io.echo()   * pluralize() is now in bbsengine6.util   * use kwargs.get()

--- src/empyre/disaster.py
+++ src/empyre/disaster.py
@@ -1,58 +1,60 @@
 import random
 
-import ttyio5 as ttyio
-import bbsengine5 as bbsengine
+from bbsengine6 import io, util
 
 from . import lib
 
-def init(args, **kw):
-    pass
-
-def access(args, op, **kw):
+def init(args, **kwargs):
     return True
+
+def access(args, op, **kwargs):
+    return True
+
+def buildargs(args=None, **kwargs):
+    return None
 
 def plague(player):
     res = []
 
     x = random.randint(0, player.serfs//4) # int(random.random()*player.serfs/4)
     if x > 0:
-        res.append("{var:empyre.highlightcolor}%s{/all}" % (bbsengine.pluralize(x, "serf", "serfs")))
+        res.append("{empyre.highlightcolor}%s{/all}" % (util.pluralize(x, "serf", "serfs")))
         player.serfs -= x
 
     x = random.randint(0, player.soldiers//2) # int(random.random()*player.soldiers/2)
     if x > 0:
         player.soldiers -= x
-        res.append("{var:empyre.highlightcolor}%s{/all}" % (bbsengine.pluralize(x, "soldier", "soldiers")))
+        res.append("{empyre.highlightcolor}%s{/all}" % (util.pluralize(x, "soldier", "soldiers")))
 
     x = random.randint(0, player.nobles//3) # int(random.random()*player.nobles/3)
     if x > 0:
         player.nobles -= x
-        res.append("{var:empyre.highlightcolor}%s{/all}" % (bbsengine.pluralize(x, "noble", "nobles")))
+        res.append("{empyre.highlightcolor}%s{/all}" % (util.pluralize(x, "noble", "nobles")))
 
     if len(res) > 0:
-        ttyio.echo("P L A G U E ! %s died" % (bbsengine.oxfordcomma(res)))
+        io.echo("P L A G U E ! %s died" % (util.oxfordcomma(res)))
 
 def rats(player):
     x = random.randint(1, player.grain//3) # int(random.random()*player.grain/3)
     player.grain -= x
-    ttyio.echo("EEEK! rats eat :crop: {var:empyre.highlightcolor}%s{/all} of grain!" % (bbsengine.pluralize(x, "bushel", "bushels")))
+    io.echo("EEEK! rats eat :crop: {empyre.highlightcolor}%s{/all} of grain!" % (util.pluralize(x, "bushel", "bushels")))
 
 def earthquake(player):
-    x = bbsengine.diceroll(100) # random.randint(1, 100))
+    x = util.diceroll(100) # random.randint(1, 100))
     if x < 85:
         return
     if player.palaces > 0 and player.nobles > 0:
         player.palaces -= 1
         player.nobles -= 1
-        ttyio.echo("EARTHQUAKE!")
-        ttyio.echo()
-        ttyio.echo("{orange}1 noble was killed{/all}")
+        io.echo("EARTHQUAKE!")
+        io.echo()
+        io.echo("{orange}1 noble was killed{/all}")
         if player.palaces == 0:
-            ttyio.echo("Your last palace has been destroyed!")
+            io.echo("Your last palace has been destroyed!")
         elif player.palaces == 1:
-            ttyio.echo("You have one palace remaining!")
+            io.echo("You have one palace remaining!")
         else:
-            ttyio.echo("One of your palaces was destroyed")
+            io.echo("One of your palaces was destroyed")
         # &"{orange}One of your Palace(s) was destroyed!{pound}$l1 noble was killed."
 
 def volcano(player):
@@ -60,35 +62,35 @@ def volcano(player):
 
     x = random.randint(0, player.markets//3) # int(random.random()*player.markets/3)
     if x > 0:
-        res.append(bbsengine.pluralize(x, "market", "markets"))
+        res.append(util.pluralize(x, "market", "markets"))
         player.markets -= x
 
     x = random.randint(0, player.mills//4) # int(random.random()*player.mills/4)
     if x > 0:
-        res.append(bbsengine.pluralize(x, "mill", "mills"))
+        res.append(util.pluralize(x, "mill", "mills"))
         player.mills -= x
 
     x = random.randint(0, player.foundries//3) # int(random.random()*player.foundries/3)
     if x > 0:
-        res.append(bbsengine.pluralize(x, "foundry", "foundries"))
+        res.append(util.pluralize(x, "foundry", "foundries"))
         player.foundries -= x
 
     if len(res) > 0:
-        ttyio.echo("Mount Apocolypse has erupted!{F6}Lava wipes out %s" % (bbsengine.oxfordcomma(res)))
+        io.echo("Mount Apocolypse has erupted!{F6}Lava wipes out %s" % (util.oxfordcomma(res)))
 
 def tidalwave(player):
     if player.shipyards > 0:
         x = random.randint(0, player.shipyards//2) # int(random.random()*player.shipyards/2)
         if x > 0:
-            ttyio.echo("TIDAL WAVE!{F6:2}{blue}{var:empyre.highlightcolor}%s under water!" % (bbsengine.pluralize(x, "shipyard is", "shipyards are")))
+#            shipyrd = player.getresource("shipyards")
+            io.echo("TIDAL WAVE!{F6:2}{blue}{empyre.highlightcolor}%s under water!" % (util.pluralize(x, "shipyard is", "shipyards are", emoji=":anchor:")))
 
-def main(args, **kw) -> bool:
-    player = kw["player"] if "player" in kw else None
-    disaster = kw["disaster"] if "disaster" in kw else bbsengine.diceroll(12)
-    if args.debug is True:
-        ttyio.echo("disaster.200: disaster=%s" % (disaster), level="debug")
+def main(args, **kwargs) -> bool:
+    player = kwargs.get("player", None)
+    disaster = kwargs.get("disaster", util.diceroll(12))
+    io.echo(f"disaster.200: {disaster=}", level="debug")
     
-    ttyio.echo("{/all}")
+    io.echo("{/all}")
 
     if disaster == 2:
         plague(player)
@@ -100,5 +102,7 @@ def main(args, **kw) -> bool:
         volcano(player)
     elif disaster == 6:
         tidalwave(player)
-    ttyio.echo("{/all}")
+    io.echo("{/all}")
+    player.adjust()
+    player.save()
     return True
