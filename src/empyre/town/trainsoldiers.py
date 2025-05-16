5291aa6df8cbb9108fe30c0e509fda50c466fcf4
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-16 19:58:42 -0400

    - empyre/town/trainsoldiers.py: warrior -> soldier for consistency; updated io.echo() call to use an f-string

--- src/empyre/town/trainsoldiers.py
+++ src/empyre/town/trainsoldiers.py
@@ -21,7 +21,7 @@ def main(args, player, **kwargs):
     lib.setarea(args, "town -> train soldiers", **kwargs)
     io.echo()
     eligible = int(player.nobles*20-player.soldiers)
-    io.echo("empyre.trainsoldiers.100: eligible=%d" % (eligible), level="debug")
+    io.echo(f"empyre.trainsoldiers.100: {eligible=}", level="debug")
     if player.serfs < 1500 or eligible > (player.serfs // 2):
         io.echo("You do not have enough serfs of training age.")
         return True
@@ -31,7 +31,7 @@ def main(args, player, **kwargs):
     #&"{f6}{lt. green}Do you want them trained (Y/N) >> ":gosub 1902
     #if a then sf=sf-wb:la=la-wb:wa=wa+wb:&"{f6:2}{pound}w2{yellow}Ok, all serfs have been trained.{f6}{pound}q1"
     # ttyio.echo("{f6}{white}You have %s requirements to be trained %s." % (bbsengine.pluralize(eligible, "serf that meets", "serfs that meet"), bbsengine.pluralize(eligible, "as a soldier", "as soldiers", quantity=False)))
-    io.echo("{f6}{normalcolor}You have %s requirments to be trained as %s." % (util.pluralize(eligible, "serf that meets", "serfs that meet"), util.pluralize(eligible, "a :military-helmet: warrior", ":military-helmet: warriors", quantity=False)))
+    io.echo("{f6}{normalcolor}You have %s requirments to be trained as %s." % (util.pluralize(eligible, "serf that meets", "serfs that meet"), util.pluralize(eligible, "a :military-helmet: soldier", ":military-helmet: soldiers", quantity=False)))
     if eligible > 0:
         io.echo("Training cost is 1 acre per serf.")
         if io.inputboolean("{promptcolor}Do you wish them trained? {optioncolor}[yN]{promptcolor}: {inputcolor}", "N") is True:
