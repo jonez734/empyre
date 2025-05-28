2f1f58bf05760ada6d90fa9de027d43871dfe595
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-27 22:55:42 -0400

    - empyre/ship/main.py: <undocumented>

--- src/empyre/ship/main.py
+++ src/empyre/ship/main.py
@@ -1,7 +1,8 @@
 from bbsengine6 import io, listbox, database, member
 
-from . import lib
-from .. import lib as empyre
+from . import lib as libship
+from .. import lib as libempyre
+from .. import player as libplayer
 
 def keyhandler(args, ch, listbox):
     currentitem = listbox.currentitem
@@ -14,8 +15,8 @@ def keyhandler(args, ch, listbox):
         io.setvar("cic", "{currentitemcolor}")
         currentitem.display()
         io.echo("{restorecursor}new ship")
-        ship = lib.build(args)
-        lib.insert(args, "__ship", ship)
+        ship = libship.build(args)
+        libship.insert(args, "__ship", ship)
         return True # key has been handled
     return False
 
@@ -28,66 +29,88 @@ def access(args, op, **kw):
 def buildargs(args=None, **kw):
     return None
 
-def main(args, **kw):
+def main(args, **kwargs):
     io.echo("ships")
     
-    player = kw["player"] if "player" in kw else None
-    location = kw["location"] if "location" in kw else "mainland"
+    def _work(conn):
+        sql = f"select moniker from empyre.ship where location=%s and playermoniker=%s"
+        dat = (location, player.moniker,)
+        with database.cursor(conn) as cur:
+            cur.execute(sql, dat)
+            if cur.rowcount == 0:
+                io.echo("building first ship")
+                libship.build(args, **kwargs)
+
+            op = libship.selectship(args, cur=cur, **kwargs)
+            if op.kind == "exit":
+                return True
+
+            ship = op.listitem.ship
+        #    ship.player = player
+
+            io.echo(f"{ship.moniker=}", level="debug")
+            if ship is None:
+                io.echo("No ship selected")
+                return True
+
+            done = False
+            while not done:
+                player.adjust()
+                player.save()
+                ship.adjust()
+                ship.save()
+                io.echo(f"{{optioncolor}}[L]{{labelcolor}} Load")
+                io.echo(f"{{optioncolor}}[U]{{labelcolor}} Unload")
+                io.echo(f"{{optioncolor}}[M]{{labelcolor}} Moniker: {ship.moniker}")
+                io.echo(f"{{optioncolor}}[S]{{labelcolor}} Sail")
+                io.echo(f"{{optioncolor}}[X]{{labelcolor}} Exit to dock")
+                
+                ch = io.inputchar("ship: {inputcolor}", "ULMSXQ", "X")
+                if ch == "Q" or ch == "X":
+                    io.echo("Exit")
+                    done = True
+                elif ch == "M":
+                    moniker = libship.inputshipname(args, "ship's moniker:", ship.moniker)
+                    if moniker == ship.moniker:
+                        io.echo("no change")
+                        continue
+                    ship.adjust()
+                    ship.save(moniker=moniker)
+                elif ch == "L":
+                    libship.runmodule(args, "load", ship=ship, **kwargs)
+                elif ch == "U":
+                    libship.runmodule(args, "unload", ship=ship, **kwargs)
+                elif ch == "S":
+                    libship.runmodule(args, "sail", ship=ship, **kwargs)
+            return True
+
+    player = kwargs.get("player", None)
+#    location = kw["location"] if "location" in kw else "mainland"
     if player is None:
         io.echo("You do not exist! Go Away!", level="error")
         return False
-    if player.ships == 0:
+    n = libship.count(args, player.moniker, **kwargs)
+    player.ships = n
+    if n == 0:
         io.echo("You have no ships!")
-        lib.newship()
+        libship.build(args, **kwargs)
         return False
 
-    location = kw["location"] if "location" in kw else "mainland"
+    if player.ships > player.shipyards*libship.SHIPSPERSHIPYARD:
+        shipyardres = player.getresource("shipyards")
+        libempyre.trade(args, player, "shipyards", **shipyardres)
+        if player.ships > player.shipyards*libship.SHIPSPERSHIPYARD:
+            io.echo("aborted")
+            return False
 
-    sql = f"select moniker from empyre.ship where location=%s and playermoniker=%s"
-    dat = (location, player.moniker,)
-    dbh = database.connect(args)
-    cur = dbh.cursor()
-    cur.execute(sql, dat)
-    if cur.rowcount == 0:
-        io.echo("building first ship")
-        lib.build(args, **kw)
+    player.adjust()
+    player.save()
 
-    op = lib.selectship(args, cur=cur, **kw)
-    if op.kind == "exit":
-        return True
+    location = kwargs.get("location", "mainland")
 
-    ship = op.listitem.ship
-#    ship.player = player
-
-    io.echo(f"{ship.moniker=}", level="debug")
-    if ship is None:
-        io.echo("No ship selected")
-        return True
-
-    done = False
-    while not done:
-        io.echo(f"{{optioncolor}}[L]{{labelcolor}} Load")
-        io.echo(f"{{optioncolor}}[U]{{labelcolor}} Unload")
-        io.echo(f"{{optioncolor}}[M]{{labelcolor}} Moniker: {ship.moniker}")
-        io.echo(f"{{optioncolor}}[S]{{labelcolor}} Scrap")
-        io.echo(f"{{optioncolor}}[X]{{labelcolor}} Exit to dock")
-        
-        ch = io.inputchar("ship: {inputcolor}", "ULMSXQ", "X")
-        if ch == "Q" or ch == "X":
-            io.echo("Exit")
-            done = True
-        elif ch == "M":
-            moniker = lib.inputshipname(args, "ship's moniker:", ship.moniker)
-            if moniker == ship.moniker:
-                io.echo("no change")
-                continue
-            ship.adjust()
-            ship.save(moniker=moniker)
-        elif ch == "L":
-            lib.runmodule(args, "load", ship=ship, **kw)
-        elif ch == "U":
-            lib.runmodule(args, "unload", ship=ship, **kw)
-        elif ch == "S":
-            lib.runmodule(args, "scrap", ship=ship, **kw)
-    return True
-
+    conn = kwargs.get("conn", None)
+    if conn is None:
+        pool = kwargs.get("pool", None)
+        with database.connect(args, pool=pool) as conn:
+            return _work(conn)
+    return _work(conn)
