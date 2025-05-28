71dcb4f958fc6445cf64a8a4069c55c953b932cc
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-28 08:21:22 -0400

    - empyre/shownews.py: upgraded to use bbsengine6

--- src/empyre/shownews.py
+++ src/empyre/shownews.py
@@ -1,21 +1,34 @@
-import ttyio5 as ttyio
-import bbsengine5 as bbsengine
+from bbsengine6 import io, util, database
 
-def access(args, op, **kw):
+def init(args, **kwargs):
     return True
 
-def init(args, **kw):
+def access(args, op, **kwargs):
     return True
 
-def main(args:object, player:object, **kw):
-    dbh = bbsengine.databaseconnect(args)
-    sql = "select * from empyre.newsentry where coalesce(dateupdated, datecreated) > %s"
-#    sql = "select * from empyre.newsentry where (extract(epoch from (coalesce(dateupdated, datecreated)))) > %s"
-    dat = (player.datelastplayed,)
-    cur = dbh.cursor()
-    cur.execute(sql, dat)
-    for entry in bbsengine.resultiter(cur, 10): # iter_news(cur, 10):
-        ttyio.echo(" %s: %s (#%s): %s" % (bbsengine.datestamp(entry["datecreated"]), entry["createdbyname"], entry["createdbyid"], entry["message"]))
+def buildargs(args, **kwargs):
+    return None
+
+def main(args:object, player:object, **kwargs):
+    pool = kwargs.get("pool", None)
+    if pool is None:
+        io.echo("empyre.shownews.100: {pool=}", level="error")
+        return False
+
+    with database.connect(args, pool=pool) as conn:
+        with database.cursor(conn) as cur:
+            sql = "select * from empyre.newsentry order by datecreated desc"
+            dat = (player.datelastplayed,)
+            cur.execute(sql)
+
+            if cur.rowcount == 0:
+                io.echo("no entries")
+                return True
+
+            util.heading("empyre news")
+
+            for entry in database.resultiter(cur, 10): # iter_news(cur, 10):
+                io.echo(f"{{var:valuecolor}}{util.datestamp(entry['datecreated'], format='%d%b%y@%H%M (%a)')} {entry['playermoniker']}: {entry['message']}")
         
-    ttyio.echo("{/all}")
+    io.echo("{/all}")
     return True
