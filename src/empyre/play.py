9ca3003907cb9c9d1e34d401059653281e8e2942
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-28 12:19:39 -0400

    - empyre/play.py:   * **kw -> **kwargs   * step through list of submodules and run them

--- src/empyre/play.py
+++ src/empyre/play.py
@@ -3,37 +3,64 @@ import time
 from datetime import datetime
 import dateutil.tz
 
-from bbsengine6 import io
+from bbsengine6 import io, member
 
 from . import lib # this means "import lib from the current package" also allows .. and ...
+
+from . import player as libplayer
 
 def init(args, **kwargs):
     return True
 
-def access(args, op, **kw):
+def access(args, op, **kwargs):
     return True
 
-def buildargs(args, **kw):
+def buildargs(args, **kwargs):
     return None
 
-def main(args, **kw):
-    player = kw["player"] if "player" in kw else None
-    if player is None:
-        io.echo("You do not exist! Go Away!")
+def main(args, **kwargs):
+    player = kwargs.get("player", None)
+    pool = kwargs.get("pool", None)
+    if pool is None:
+        io.echo(f"empyre.play.main.300: {pool=}", level="error")
         return False
 
+#    currentmoniker = member.getcurrentmoniker(args, **kwargs)
+#    playercount = libplayer.count(args, currentmoniker, **kwargs)
+#    io.echo(f"empyre.main.100: {playercount=}", level="debug")
+#    if playercount is None:
+#        player = libplayer.create(args, pool=pool)
+#        if player is None:
+#            io.echo("empyre.main.200: unable to create new player!", level="error")
+#            return False
+#    elif playercount > 0:
+#        io.echo(f"empyre.main.220: {pool=}", level="debug")
+#        player = libplayer.select(args, currentmoniker, **kwargs)
+#        if player is None:
+#            return False
+
+#    player = kw["player"] if "player" in kw else None
+#    if player is None:
+#        io.echo("You do not exist! Go Away!")
+#        return False
+
+#    io.echo(f"empyre.play.main.200: {player.resources['grain']=} {player.grain=} {player.attributes=}")
 #    player.datelastplayedepoch = time.mktime(time.localtime())
     tzlocal = dateutil.tz.tzlocal()
     player.datelastplayed = datetime.now(tzlocal)
     
-    if lib.runmodule(args, "sysopoptions", **kw) is False:
-        io.echo("error running module 'sysopoptions'", level="warn")
+    io.echo(f"empyre.play.200: checking for sysop options... {kwargs=}")
+    if lib.runmodule(args, "sysopoptions", **kwargs) is False:
+        io.echo(f"failed to run module", level="error")
+    else:
+        io.echo(" ok ", level="ok")
 
     io.echo("{f6}{cyan}it is a new year...{/all}")
-
-    if player.turncount >= lib.TURNSPERDAY:
+#    io.echo(f"short circuit", level="info")
+#    return True
+    if player.turncount >= libplayer.TURNSPERDAY:
         io.echo("{red}The other rulers unite against you for hogging the game!{/all}")
-        player.turncount = lib.TURNSPERDAY
+        player.turncount = libplayer.TURNSPERDAY
         player.save()
         return True
     
@@ -46,31 +73,15 @@ def main(args, **kw):
     player.save()
     io.echo(f"{player.datelastplayed=}", level="debug")
 
-    for x in ("dock",): # town.naturaldisasterbank",): # "yearlyreport",):
-#    for x in ("weather", "disaster", "harvest", "colonytrip", "town", "combat", "dock", "quests", "investments"):
+    for x in ("dock",):# "investments"):
+#    for x in ("weather", "disaster", "colonytrip", "harvest", "town", "combat", "dock", "investments", "yearlyreport"): # colonytrip after disaster; dock, quests after combat?
         if args.debug is True:
             io.echo(f"play.100: {x=}", level="debug")
-        if lib.runmodule(args, x, **kw) is False:
+        if lib.runmodule(args, x, **kwargs) is False:
             io.echo("error running submodule %r" % (x), level="error")
         player.adjust()
         player.save()
 
-    io.echo("end turn...{F6}")
-    
-    if player.serfs < 100:
-        player.beheaded = True
-        player.memberid = None
-        player.adjust()
-        player.save(force=True)
-        io.echo("{normalcolor}You haven't enough serfs to maintain the empyre! It's turned over to King George and you are {highlightcolor}beheaded{normalcolor}.{/all}")
-        return
-
-    player.adjust()
-    player.save()
-
-    if lib.runmodule(args, "yearlyreport", **kw) is not True:
-        io.echo("error running 'yearlyreport' submodule.", level="debug")
-    
     io.echo("turn complete!", level="success")
     io.echo("{/all}")
 
