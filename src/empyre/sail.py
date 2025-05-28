7bbed269102fe19885d3ed28442dd9594b406b07
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-28 08:38:16 -0400

    - empyre/sail.py: **kw -> **kwargs

--- src/empyre/sail.py
+++ src/empyre/sail.py
@@ -1,16 +1,20 @@
 # @since 20220803 created 'sail' module
 
-import ttyio5 as ttyio
-import bbsengine5 as bbsengine
-
-
+from bbsengine6 import io, util
 from . import lib
 
-def access(args, op, **kw):
+def access(args, op, **kwargs):
     return
 
-def init(args, **kw):
+def init(args, **kwargs):
     return
 
-def main(args, **kw):
+def main(args, **kwargs):
+    bbsengine.util.heading("sail")
+    io.echo(f"setting sail {kwargs=}", level="debug")
+
+    ship = kwargs.get("ship", None)
+    if ship is None:
+        io.echo("ship must be specified", level="error")
+        return False
     return
