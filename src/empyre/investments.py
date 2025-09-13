cefdd4c4002b248a4177c4e14cfdc3c7a1eefffd
Jeff MacDonald <jam@zoidtechnologies.com>
2025-09-13 17:20:28 -0400

    - empyre/investments.py: kw -> kwargs and renamed investopts to investmentoptions

--- src/empyre/investments.py
+++ src/empyre/investments.py
@@ -9,15 +9,16 @@ def buildinvestmentoptions(player):
 
     investmentoptions = {}
     index = 0
-    for a in player.resources:
-        if "price" in a and a["price"] > 0:
-            investmentoptions[chr(65+index)] = a
+    for name, data in player.resources.items():
+        if "price" in data and data["price"] > 0:
+            data["name"] = name
+            investmentoptions[chr(65+index)] = data
             index += 1
     return investmentoptions
 
 def displayinvestmentoptions(investmentoptions): # opts, player):
     maxlen = 0
-    for ch, a in investopts.items(): # player.attributes:
+    for ch, a in investmentoptions.items(): # player.attributes:
         name = a["name"] if "name" in a else ""
         if len(name) > maxlen:
             maxlen = len(name)
@@ -37,7 +38,7 @@ def displayinvestmentoptions(investmentoptions): # opts, player):
 def init(args, **kw):
     return True
 
-def help(**kw:dict) -> None:
+def investmentshelp(**kw:dict) -> None:
     player = kw["player"] if "player" in kw else None
     investmentoptions = buildinvestmentoptions(player)
     return displayinvestmentoptions(investmentoptions)
@@ -66,8 +67,8 @@ def main(args, **kw):
 
     done = False
     while not done:
-        prompt = "{{promptcolor}}{util.pluralize(player.coins, 'coin', 'coins', emoji=':moneybag:')}{{f6}}Investments {{optioncolor}}[{options}]{{promptcolor}}: {{inputcolor}}"
-        ch = io.inputchar(prompt, options, "Q", help=help, args=args, player=player)
+        prompt = f"{{promptcolor}}{util.pluralize(player.coins, 'coin', 'coins', emoji=':moneybag:')}{{f6}}Investments {{optioncolor}}[{options}]{{promptcolor}}: {{inputcolor}}"
+        ch = io.inputchar(prompt, options, "Q", help=investmentshelp, args=args, player=player)
         if ch == "Q":
             io.echo(f"{{optioncolor}}Q{{labelcolor}} -- Quit")
             done = True
@@ -80,15 +81,15 @@ def main(args, **kw):
             io.echo(f"{{optioncolor}}Y{{labelcolor}} -- Your Stats")
             player.status()
         else:
-            for opt, r in investopts.items():
+            for opt, r in investmentoptions.items():
                 if ch == opt:
                     name = r["name"]
                     price = r["price"]
-                    attr = r["name"]
                     singular = r["singular"] if "singular" in r else "singular"
                     plural = r["plural"] if "plural" in r else "plural"
                     io.echo(f"{{optioncolor}}{ch}{{labelcolor}} -- {name.title()} {util.pluralize(price, 'coin', 'coins', emoji=':moneybag:')} each")
-                    trade(args, player, attr, name, price, singular, plural)
+                    res = player.getresource(name)
+                    libempyre.trade(args, player, **res)
                     break
             else:
                 io.echo("{optioncolor}%r{labelcolor} -- not implemented yet" % (ch))
