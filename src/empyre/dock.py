e7d077b2d4dfe62a693c91de40a7e4443f807570
Jeff MacDonald <jam@zoidtechnologies.com>
2025-09-15 16:49:07 -0400

    - empyre/dock.py:   * **kw -> **kwargs   * untested, subject to further rewrites

--- src/empyre/dock.py
+++ src/empyre/dock.py
@@ -1,63 +1,92 @@
 # @since 20220803 created 'drydock' module @see mdl.emp.delx2.txt#50277
+# @since 20231222 renamed 'drydock' to 'dock'
+#import bbsengine6 as bbsengine
+from bbsengine6 import io, util
 
-import ttyio6 as ttyio
-import bbsengine6 as bbsengine
+from . import lib as libempyre
+from .ship import lib as libship
 
-from . import lib
+def scanshiplocations(args, location):
+    sql = "select * from empyre.ship where ownerid=%s and location=%s"
+    dat = (currentplayerid, location)
 
-def init(args, **kw):
+def init(args, **kwargs):
     return True
 
-def access(args, op, **kw):
+def access(args, op, **kwargs):
     return True
 
-def buildargs(args, **kw):
+def buildargs(args, **kwargs):
     return None
 
-def main(args, **kw):
-    player = kw["player"] if "player" in kw else None
+def dockhelp(**kwargs):
+    io.echo("{var:optioncolor}[T]{var:labelcolor} Trade")
+    io.echo("{var:optioncolor}[S]{var:labelcolor} Ship :ship:")
+    io.echo("{var:optioncolor}[X]{var:labelcolor} Exit{var:normalcolor} :door:")
+    return True
 
+def trade(args, **kwargs):
+    player = kwargs.get("player", None)
+
+    yrd = player.getresource("shipyards")
+    libempyre.trade(args, player, "shipyards", **yrd)
+
+#    shp = player.getresource("ships")
+#    libempyre.trade(args, player, "ships", **shp)
+
+    nav = player.getresource("navigators")
+    libempyre.trade(args, player, "navigators", **nav)
+
+    player.adjust()
+    player.save()
+
+def main(args, **kwargs):
+    player = kwargs.get("player", None)
     if player is None:
-        ttyio.echo("you do not exist! go away!", level="error")
+        io.echo("you do not exist! go away!", level="error")
         return False
-    def help(**kw):
-        ttyio.echo(":compass: {var:optioncolor}[R]{var:labelcolor}ecruit navigator{var:normalcolor}") # show how many are needed per ship
-        ttyio.echo(":package: {var:optioncolor}[E]{var:labelcolor}xports{var:normalcolor}")
-        ttyio.echo(":anchor: {var:optioncolor}[S]{var:labelcolor}hips{var:normalcolor}")
-        ttyio.echo(" {var:optioncolor}ship[Y]ards{var:normalcolor}")
-        ttyio.echo("{f6}:door: {var:optioncolor}[Q]{var:labelcolor}uit{var:normalcolor}")
+
+    libempyre.setarea(args, "dock", player=player)
+
+    io.echo("checking ship count..: ", end="")
+    s = libship.count(args, playermoniker=player.moniker, **kwargs)
+    io.echo(f"{s=}")
+
+    if player.ships != s:
+        player.ships = s
+        player.adjust()
+        player.save()
 
     done = False
     while not done:
-        bbsengine.util.heading("dry dock")
-        lib.setarea(args, player, "dry dock")
-        help()
-        ttyio.echo("You have %s and %s" % (bbsengine.pluralize(player.ships, "ship", "ships", emoji=":anchor:"), bbsengine.pluralize(player.navigators, "navigator", "navigators", emoji=":compass:")))
-        ch = ttyio.inputchar("{var:promptcolor}dry dock: {var:inputcolor}", "RESQ", "Q", help=help)
-        if ch == "Q":
+        util.heading("dock")
+        dockhelp()
+        shp = player.getresource("ships")
+        nav = player.getresource("navigators")
+        yrd = player.getresource("shipyards")
+        io.echo(f"You have {util.pluralize(player.ships, **shp)}, {util.pluralize(player.shipyards, **yrd)}, and {util.pluralize(player.navigators, **nav)}")
+        ch = io.inputchar("{var:promptcolor}dock: {var:inputcolor}", "STQX", "X", help=dockhelp)
+        if ch == "Q" or ch == "X":
+            io.echo("exit")
             done = True
             break
-        elif ch == "E":
-            ttyio.echo("Exports{/all}")
+        elif ch == "T":
+            trade(args, **kwargs)
         elif ch == "R":
-            ttyio.echo("Recuit Navigator{/all}")
+            io.echo("Recuit Navigator{/all}")
             if player.ships == 0:
-                ttyio.echo("You do not have any ships.")
-            elif player.navigators <= player.ships:
+                io.echo("You do not have any ships.")
+            elif player.navigators <= player.ships and player.ships > 0:
                 need = abs(player.ships - player.navigators)
                 if need > 0:
-                    ttyio.echo("You need %s to outfit your %s." % (bbsengine.util.pluralize(player.navigators, "navigator", "navigators", emoji=":compass:"), bbsengine.util.pluralize(player.ships, "ship", "ships", emoji=":anchor:")))
-                    lib.trade(args, player, "navigators", "navigators", price=500, singular="navigator", plural="navigators", emoji=":compass:")
+#                    nav = player.getresource("navigators")
+#                    shp = player.getresource("ships")
+                    io.echo(f"You need {util.pluralize(need, **nav)} to outfit your {util.pluralize(player.ships, **shp)}.")
+                    libempyre.trade(args, player, "navigators", "navigators", **nav)
         elif ch == "S":
-            ttyio.echo("Ships{/all}")
-            if player.shipyards > 0:
-                lib.trade(args, player, "ships", "ships", price=5000, singular="ship", plural="ships", emoji=":anchor:")
-            else:
-                ttyio.echo("You do not have any shipyards.")
-        elif ch == "Y":
-            ttyio.echo("Shipyards")
-            a = player.getattribute("shipyards")
-
-            lib.trade(args, player, "shipyards", "shipyards", price=a["price"], singular=a["singular"], plural=a["plural"])
+            io.echo("Ships{/all}")
+            libempyre.runmodule(args, "ship", location="mainland", **kwargs)
+        else:
+            io.echo("{bell}")
 
     return True
