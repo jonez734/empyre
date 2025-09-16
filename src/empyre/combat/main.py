80018118793777e4c11d55e8e75250299aeee0d1
Jeff MacDonald <jam@zoidtechnologies.com>
2025-09-16 19:19:47 -0400

    - empyre/combat/main.py:   * help() -> combathelp()   * added the 'attack yourself' check   * removed code used for testing which generates an NPC   * menu -> MENU

--- src/empyre/combat/main.py
+++ src/empyre/combat/main.py
@@ -3,6 +3,7 @@ from bbsengine6 import io, util
 from . import lib
 
 from .. import lib as libempyre
+from .. import player as libplayer
 
 def init(args, **kw):
     return True
@@ -13,51 +14,43 @@ def access(args, op, **kw):
 def buildargs(args, **kw):
     return None
 
-def main(args, **kw):
-    menu = (
-        {"hotkey": "1", "label": "Attack Army",   "callback": "attackarmy"},
-        {"hotkey": "2", "label": "Attack Palace", "callback": "attackpalace"},
-        {"hotkey": "3", "label": "Attack Nobles", "callback": "attacknobles"},
-        {"hotkey": "4", "label": "Send Diplomat", "callback": "senddiplomat"},
-        {"hotkey": "5", "label": "Joust",         "callback": "joust"}
-    )
-    player = kw["player"] if "player" in kw else None
+MENU = (
+    {"hotkey": "1", "label": "Attack Army",   "callback": "attackarmy"},
+    {"hotkey": "2", "label": "Attack Palace", "callback": "attackpalace"},
+    {"hotkey": "3", "label": "Attack Nobles", "callback": "attacknobles"},
+    {"hotkey": "4", "label": "Send Diplomat", "callback": "senddiplomat"},
+    {"hotkey": "5", "label": "Joust",         "callback": "joust"}
+)
+
+def main(args, **kwargs):
+    player = kwargs["player"] if "player" in kwargs else None
     otherplayer = None
 
     def sneakattack():
         pass
 
-    def help():
+    def combathelp(**kwargs):
         util.heading("Combat Menu")
-        for m in menu:
+        for m in MENU:
             io.echo(f"{{var:optioncolor}}[{m['hotkey']}]{{var:labelcolor}} {m['label']}")
         return
 
-    # @todo: len(otherplayers) > 0: roll 1d<len+1>; generate NPC if x = len+1
-    #otherplayerrank = random.randint(0, min(3, player.rank + 1))
-    #otherplayer = Player(args, npc=True)
-    #otherplayer.generate(otherplayerrank)
-    #otherplayer.playerid = otherplayer.insert()
-    #otherplayer.status()
-    #otherplayer.dbh.commit()
+    libempyre.setarea(args, "combat: select opponent", player=player)
 
-    libempyre.setarea(args, player, "combat: select opponent")
-
-    otherplayer = libempyre.selectplayer(args, title="select opponent", prompt="opponent: ")
-    #libempyre.inputplayername("Attack Whom? >> ", multiple=False, noneok=True, args=args) # , verify=verifyOpponent)
-    if otherplayer is None:
+    otherplayer = libplayer.select(args, title="select opponent", prompt="opponent: ", **kwargs)
+    if otherplayer is False:
+        io.echo("failed to select opponent", level="error")
+        return False
+    elif otherplayer is None:
         io.echo("no attack.", level="info")
-        return
+        return None
 
-#    otherplayerid = libempyre.getplayerid(args, otherplayername)
+    if player.moniker == otherplayer.moniker:
+        if io.inputboolean(f"{{var:promptcolor}}it is unwise to attack yourself. are you sure?: {{var:inputcolor}}", "N") is False:
+            io.echo("{f6}aborted{f6}")
+            return True
 
-#    otherplayer = libempyre.Player(args)
-#    otherplayer.load(otherplayerid)
-#    if otherplayer is None:
-#        io.echo("invalid player id. aborted.", level="error")
-#        return False
-
-    help()
+    combathelp()
 
     done = False
     while not done:
@@ -70,16 +63,16 @@ def main(args, **kw):
         libempyre.setarea(args, "combat", help=True, player=player)
 
         # @see empire6/mdl.emp.delx3.txt#L91
-        ch = io.inputchar("{var:promptcolor}Combat Command {var:optioncolor}[1-6,?,Q]{var:promptcolor}: {var:inputcolor}", "123456Q?", "Q", help=help)
+        ch = io.inputchar("{var:promptcolor}Combat Command {var:optioncolor}[1-6,?,Q]{var:promptcolor}: {var:inputcolor}", "123456Q?", "Q", help=combathelp)
         i = ord(ch)-ord("1")
         if ch in ("1", "2", "3", "4", "5"):
-            io.echo(f"{{var:inputcolor}}{menu[i]['label']}")
-            if lib.runmodule(args, menu[i]["callback"], otherplayer=otherplayer, **kw) is False:
+            io.echo(f"{{var:inputcolor}}{MENU[i]['label']}")
+            if lib.runmodule(args, MENU[i]["callback"], otherplayer=otherplayer, **kwargs) is False:
                 io.echo("attack failed (module error)", level="error")
                 continue
         elif ch == "?" or ch == "KEY_HELP":
             io.echo("{var:inputcolor}Help")
-            help()
+            combathelp()
         elif ch == "Q" or ch == "6":
             io.echo("{var:inputcolor}Cease Fighting")
             done = True
