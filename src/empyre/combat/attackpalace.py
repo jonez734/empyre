3dc6910de95dee38e7649992be18074087938a5d
Jeff MacDonald <jam@zoidtechnologies.com>
2025-09-16 11:19:22 -0400

    - empyre/combat/attackpalace.py: fixed return values for value checks like 'no palaces' and such

--- src/empyre/combat/attackpalace.py
+++ src/empyre/combat/attackpalace.py
@@ -2,26 +2,29 @@ import random
 
 from bbsengine6 import io, util
 
-def init(args, **kw):
+def init(args, **kwargs):
     return True
 
-def access(args, op, **kw):
+def access(args, op, **kwargs):
     return True
 
-# @see https://github.com/Pinacolada64/ImageBBS/blob/e9f033af1f0b341d0d435ee23def7120821c3960/v1.2/games/empire6/plus_emp6_combat.lbl#L105
+def buildargs(args, **kwargs):
+    return None
+
+# @see empire6/plus_emp6_combat.lbl#L105
 def main(args, **kwargs):
-    player = kwargs["player"] if "player" in kwargs else None
-    otherplayer = kwargs["otherplayer"] if "otherplayer" in kwargs else None
+    player = kwargs.get("player", None)
+    otherplayer = kwargs.get("otherplayer", None)
 
     if otherplayer.palaces < 1:
         io.echo("They have no palaces!")
-        return False
+        return True
 
     if player.soldiers < 1:
         io.echo("You have no soldiers!")
-        return False
+        return True
 
-    # https://github.com/Pinacolada64/ImageBBS/blob/e9f033af1f0b341d0d435ee23def7120821c3960/v1.2/games/empire6/plus_emp6_combat.lbl#L108
+    # empire6/plus_emp6_combat.lbl#L108
     io.echo("{F6:2}You attack an enemy palace...")
     if random.random()*player.soldiers < random.random()*otherplayer.soldiers*3:
         io.echo("{F6}{lightblue}Guards appear and thwart your attempt!")
@@ -30,7 +33,7 @@ def main(args, **kwargs):
         io.echo("{F6:2}{white}--> {lightblue}The guards kill %s{F6}" % (util.pluralize(soldierslost, **res)))
         player.soldiers -= soldierslost # sl
         io.echo("{/all}")
-        return False
+        return True
     io.echo("{F6}You destroyed one of their palaces!{f6}")
     soldierslost = random.randint(1, player.soldiers//2)
     if otherplayer.nobles > 0:
